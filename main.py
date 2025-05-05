c5c5fb7dbf4ad8fb096bc133047fc6119a8772e0
mr-melenas <max92sba@hotmail.com>
2025-05-05 17:30:02 +0200

    feat: create new base for api

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 from fastapi.responses import HTMLResponse
+
+#inicializamos la app
+app = FastAPI()
+
+# rutas static y templates
+templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/", response_class=HTMLResponse)
+async def root(request: Request):
+    return templates.TemplateResponse(request, "index.html", 
+        {
+            "request": request, 
+            "title":  "",
+            "description": "",
+            "api_prefix": "",
+        }
+    )
